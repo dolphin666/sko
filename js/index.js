c28963863e4de2aa433d167d3e5daefc4d1d5f72
dolphin369 <479019769@qq.com>
2018-01-30 02:05:15 +0800

    2018/1/30   2:04am sko

--- js/index.js
+++ js/index.js
@@ -1,13 +1,19 @@
 /* 首页banner */
 
-// window.onload = function () {
-//     var oa = $(window).height();
-//     $(".index_banner .slide").height(oa);
-// }
+window.onload = function () {
+    var oa = $(window).height();
+    $(".index_banner .slide").height(oa);
+}
 /* 视频控制 */
 $(document).ready(function () {
-    $("#home_slider .vol").click(function () {
-        $("#banner_video").muted=true;
+    $("#home_slider i.vol").click(function () {
+        console.log("11");
+        $(this).children("video").paused=true;
         }
     );
 });
+/*myVid=document.getElementById("home_slider");
+function enableMute()
+{
+    myVid.muted=true;
+}*/
