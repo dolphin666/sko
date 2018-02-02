839f45d577ba0ae3ccf7d7e41a49355597bbf71a
dolphin666 <479019769zxx@gmail.com>
2018-02-02 18:15:08 +0800

    2018/2/2 18:14pm sko品牌

--- js/header.js
+++ js/header.js
@@ -8,3 +8,25 @@ $(function(){
         $(this).children('.sub-nav').stop().slideUp();
     });
 });
+$(function () {
+    // $(".return-webtop").on('click', function () {
+    //     $("body,html").stop().animate({scrollTop: 0}, 500);
+
+  $(".r_float div.litel").hover(function () {
+        var _this = $(this);
+        _this.removeClass("hover").stop().animate({left: -184}, 400)
+    }, function () {
+        var _this = $(this);
+        _this.stop().animate({left: 0}, 400, function () {
+            _this.addClass("hover")
+        })
+    })
+    $(".r_float div.lia").hover(function () {
+        $(this).removeClass("hover").stop().animate({marginLeft: -10}, 400)
+    }, function () {
+        var _this = $(this);
+        _this.stop().animate({marginLeft: 0}, 400, function () {
+            _this.addClass("hover")
+        })
+    })
+})
